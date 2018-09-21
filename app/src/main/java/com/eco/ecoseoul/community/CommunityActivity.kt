84b1539791fa7e4db0d03fe18bde260c94ef7921
SERIN <aloha_rini@naver.com>
2018-09-21 17:37:10 +0900

    Add community and dialog

---
+++ app/src/main/java/com/eco/ecoseoul/community/CommunityActivity.kt
@@ -0,0 +1,21 @@
+package com.serin.timiz_test
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.eco.ecoseoul.R
+import com.eco.ecoseoul.community.CommunityWriteActivity
+import kotlinx.android.synthetic.main.activity_community.*
+
+class CommunityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community)
+
+        com_write_btn.setOnClickListener {
+            val intent = Intent(this, CommunityWriteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
